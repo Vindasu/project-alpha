e02382d22712851752270391b73ee171cc791292
Alex <alexleeadams@aol.com>
2022-06-18 21:37:04 -0500

    Feature 14 completed - 13 skipped(come back)

--- projects/urls.py
+++ projects/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from projects.views import show_projects
+from projects.views import show_projects, show_details, show_create_project
 
-urlpatterns = [path("", show_projects, name="list_projects")]
+urlpatterns = [
+    path("", show_projects, name="list_projects"),
+    path("<int:pk>/", show_details, name="show_project"),
+    path("create/", show_create_project, name="create_project"),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
+from projects.forms import ProjectForm
 
 # Create your views here.
 
@@ -10,3 +11,25 @@ def show_projects(request):
     Model = Project.objects.filter(members=request.user)
     context = {"list_projects": Model}
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def show_details(request, project_id):
+    context = Project.objects.get(project_id)
+    return render(request, "projects/details.html", context, pk=project_id)
+
+
+@login_required
+def show_create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.save()
+            return redirect("show_projects")
+    else:
+        form = ProjectForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "projects/create.html", context)
