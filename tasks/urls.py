d1b471e28c8798b1ac3890ffbc9f176c6ad32460
Alex <alexleeadams@aol.com>
2022-06-19 00:11:51 -0500

    Feature 17 complete skipped 13

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from tasks.views import show_create_task, show_tasks
+from tasks.views import show_create_task, show_tasks, TaskUpdateView
+
 
 urlpatterns = [
     path("create/", show_create_task, name="create_task"),
     path("mine/", show_tasks, name="show_my_tasks"),
+    path("<int:pk>/complete/", TaskUpdateView.as_view(), name="complete_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from tasks.forms import TaskForm
 from django.contrib.auth.decorators import login_required
 from tasks.models import Task
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = "tasks/list.html"
+    fields = ["is_completed"]
+    success_url = reverse_lazy("show_my_tasks")
 
 
 @login_required
@@ -27,3 +36,18 @@ def show_tasks(request):
     Model = Task.objects.filter(assignee=request.user)
     context = {"tasks": Model}
     return render(request, "tasks/list.html", context)
+
+
+# @login_required
+# def show_update(request, pk):
+#     status = Task.objects.get(is_completed=False)
+#     form = TaskForm(instance=status)
+#     if request.method == "POST":
+#         form = TaskForm(request.POST, instance=status)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("show_my_tasks")
+#         else:
+#             form = TaskForm()
+#     context = {"form": form}
+#     return render(request, "tasks/list.html", context)
