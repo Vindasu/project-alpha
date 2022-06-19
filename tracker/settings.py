29f0161eeea3cfe89ca08c815fbce214c93e486d
Alex <alexleeadams@aol.com>
2022-06-19 00:39:51 -0500

    Feature 18 complete skipped 13

--- tracker/settings.py
+++ tracker/settings.py
@@ -16,7 +16,7 @@ LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+MARKDOWNIFY = {"default": {"BLEACH": False}}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
